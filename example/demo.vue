9cfd761898e4b02231a12ec2ffce554a975840ce
cole <bernankez@qq.com>
2022-11-12 22:27:58 +0800

    chore: add example

---
+++ example/demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>test</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Demo",
+  setup() {
+    const a = 1;
+  },
+});
+</script>
---
+++ example/index.ts
@@ -0,0 +1,4 @@
+import Demo from "./demo.vue";
+console.log(Demo);
+console.log(Demo.name);
+console.log("11");
