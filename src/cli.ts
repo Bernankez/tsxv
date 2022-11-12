d05cac39cc2f1d9a33978a30c1069ddf22e915e7
cole <bernankez@qq.com>
2022-11-13 00:28:36 +0800

    fix: fix cli

--- src/cli.ts
+++ src/cli.ts
@@ -14,82 +14,80 @@ const tsxFlags = {
   },
 };
 
-export function createCli() {
-  return cli({
-    name: "tsxv",
-    parameters: ["[script path]"],
-    flags: {
-      ...tsxFlags,
-      version: {
-        type: Boolean,
-        alias: "v",
-        description: "Show version",
-      },
-      help: {
-        type: Boolean,
-        alias: "h",
-        description: "Show help",
-      },
+cli({
+  name: "tsxv",
+  parameters: ["[script path]"],
+  flags: {
+    ...tsxFlags,
+    version: {
+      type: Boolean,
+      alias: "v",
+      description: "Show version",
     },
-    help: false,
-    ignoreArgv: ignoreAfterArgument(),
-  }, (argv) => {
-    if (argv.flags.version) {
-      process.stdout.write(`tsxv v${version}\nnode `);
-    }
-    else if (argv.flags.help) {
-      argv.showHelp({
-        description: "Node.js runtime enhanced with esbuild for loading TypeScript & ESM",
-      });
-      console.log(`${"-".repeat(45)}\n`);
-    }
+    help: {
+      type: Boolean,
+      alias: "h",
+      description: "Show help",
+    },
+  },
+  help: false,
+  ignoreArgv: ignoreAfterArgument(),
+}, (argv) => {
+  if (argv.flags.version) {
+    process.stdout.write(`tsxv v${version}\nnode `);
+  }
+  else if (argv.flags.help) {
+    argv.showHelp({
+      description: "Node.js runtime enhanced with esbuild for loading TypeScript & ESM",
+    });
+    console.log(`${"-".repeat(45)}\n`);
+  }
 
-    const childProcess = run(
-      removeArgvFlags(tsxFlags),
-      {
-        noCache: Boolean(argv.flags.noCache),
-        tsconfigPath: argv.flags.tsconfig,
-      },
-    );
+  const childProcess = run(
+    removeArgvFlags(tsxFlags),
+    {
+      noCache: Boolean(argv.flags.noCache),
+      tsconfigPath: argv.flags.tsconfig,
+    },
+  );
 
-    const relaySignal = async (signal: NodeJS.Signals) => {
-      const message = await Promise.race([
-        /**
+  const relaySignal = async (signal: NodeJS.Signals) => {
+    const message = await Promise.race([
+      /**
          * If child received a signal, it detected a keypress or
          * was sent a signal via process group.
          *
         * Ignore it and let child handle it.
         */
-        new Promise<NodeJS.Signals>((resolve) => {
-          function onKillSignal(data: { type: string; signal: NodeJS.Signals }) {
-            if (data && data.type === "kill") {
-              resolve(data.signal);
-              childProcess.off("message", onKillSignal);
-            }
+      new Promise<NodeJS.Signals>((resolve) => {
+        function onKillSignal(data: { type: string; signal: NodeJS.Signals }) {
+          if (data && data.type === "kill") {
+            resolve(data.signal);
+            childProcess.off("message", onKillSignal);
           }
+        }
 
-          childProcess.on("message", onKillSignal);
-        }),
-        new Promise((resolve) => {
-          setTimeout(resolve, 10);
-        }),
-      ]);
+        childProcess.on("message", onKillSignal);
+      }),
+      new Promise((resolve) => {
+        setTimeout(resolve, 10);
+      }),
+    ]);
 
-      /**
+    /**
        * If child didn't receive a signal, it was sent to the parent
        * directly via kill PID. Relay it to child.
        */
-      if (!message)
-        childProcess.kill(signal);
-    };
+    if (!message)
+      childProcess.kill(signal);
+  };
 
-    process.on("SIGINT", relaySignal);
-    process.on("SIGTERM", relaySignal);
+  process.on("SIGINT", relaySignal);
+  process.on("SIGTERM", relaySignal);
 
-    childProcess.on(
-      "close",
-      (code: number) => process.exit(code!),
-    );
-  });
-}
+  childProcess.on(
+    "close",
+    (code: number) => process.exit(code!),
+  );
+});
 
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { createCli } from "./cli";
-
-createCli();
